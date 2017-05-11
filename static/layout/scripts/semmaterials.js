915906cdf8807d53ca973097aa7dd8ff8e687195
namanbakhru <namanbakhru@gmail.com>
2017-05-11 20:00:05 +0530

    Download Buttons

--- static/layout/scripts/semmaterials.js
+++ static/layout/scripts/semmaterials.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
+	
 	$(".semmaterials").click(function(){
 		$(".hide").hide();
 		$(".materialselect").show();
 	});
+
+	$(".level1 button").click(function(){
+		$(".level1 button").css({"backgroundColor":"#562351", "borderStyle":"none", "color":"white"});
+		$(this).css({"backgroundColor":"white", "border":"1px solid rgb(71,71,71)", "color":"rgb(71,71,71)"});
+		$(".materialselect .level2").show();
+		$(".materialselect .level3").hide();
+	});
+
+	$(".level2 button").click(function(){
+		$(".level2 button").css({"backgroundColor":"#562351", "borderStyle":"none", "color":"white"});
+		$(this).css({"backgroundColor":"white", "border":"1px solid rgb(71,71,71)", "color":"rgb(71,71,71)"});
+		$(".materialselect .level3").show();
+	});
+
+	$("#closematerialselect").click(function(){
+		$(".level1 button").css({"backgroundColor":"#562351", "borderStyle":"none", "color":"white"});
+		$(".level2 button").css({"backgroundColor":"#562351", "borderStyle":"none", "color":"white"});
+		$(".materialselect .level3").hide();
+		$(".materialselect .level2").hide();
+		$(".hide").show();
+		$(".materialselect").hide();
+	});
 });
-$()
