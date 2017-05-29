42bd6f9affd0ba508d13b7e3b555e8c5be24a84f
namanbakhru <namanbakhru@gmail.com>
2017-05-29 15:27:10 +0530

    First Attempt for Upload System

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 @app.route("/")
 def main():
