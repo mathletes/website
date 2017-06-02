c2f20af841839daafe6b7077b2793426cabf51fc
namanbakhru <namanbakhru@gmail.com>
2017-06-02 16:00:08 +0530

    Frontend for Login Form

---
+++ static/layout/scripts/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$("#loginlink").click(function(){
+		$("#logindiv").slideDown(200);
+	});
+
+	$("#closelogin").click(function(){
+		$("#logindiv").slideUp();
+	});
+
+});
